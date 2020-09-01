40ea2dfcc106e199128ceac6ee50de6fdbc99766
MartKac <mkac003@gmail.com>
2020-09-01 21:35:09 +0200

    added the src dir

---
+++ src/Wall.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from Actor import *
+
+
+class Wall(Actor):
+  '''
+  Class for creating wals in the scene
+  '''
+  
+  def Render(self, transparent=False):
+    
+    if transparent:
+      print("aaa")
+      self.box = self.canvas.create_rectangle(self.x, self.y, self.x+self.w, self.y+self.h, fill="", outline="")
+      return
+    
+    if self.startimage is not None:
+      '''
+      If the image is specified,
+      make sprite with image.
+      '''
+      
+      img = self.startimage
+      self.box = self.canvas.create_image(self.x, self.y, image=img)
+      
+    else:
+      '''
+      if image is not specified, then make
+      rectangle that is 10x10 and color it
+      red.
+      '''
+      
+      self.box = self.canvas.create_rectangle(self.x, self.y, self.x+self.w, self.y+self.h, fill="orange", outline="")
+    
